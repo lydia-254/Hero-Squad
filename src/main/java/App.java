b367c1d01f67d540844ea332a3798c11ba9e2d04
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-06-10 23:56:35 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,17 @@
+import models.Hero;
+import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.*;
 public class App {
-}
+    public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
