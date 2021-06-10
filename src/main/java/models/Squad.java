16197240fe1ab10cceb813d893c1539464441389
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-06-11 00:15:42 +0300

    Update Squad.java

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -12,3 +12,20 @@ public class Squad {
                 Objects.equals(squadName, squad.squadName) &&
                 Objects.equals(cause, squad.cause);
     }
+    @Override
+    public int hashCode() {
+        return Objects.hash(squadName, maxNum, cause, id);
+    }
+    private String squadName;
+    private int maxNum;
+    private String cause;
+    private int id;
+    private static ArrayList<Squad> squads = new ArrayList<>();
+    //
+//
+    public Squad(String squadName ,int maxNum,String cause){
+        this.squadName=squadName;
+        this.cause=cause;
+        this.maxNum=maxNum;
+        squads.add(this);
+    }
