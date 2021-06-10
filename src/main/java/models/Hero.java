0742e236368df77b59d4e21a1d73a3b6e3e926a6
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-06-11 00:05:46 +0300

    Update Hero.java

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,15 @@
 package models;
-
+import java.util.ArrayList;
+import java.util.Objects;
 public class Hero {
-}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return age == hero.age &&
+                id == hero.id &&
+                Objects.equals(name, hero.name) &&
+                Objects.equals(power, hero.power) &&
+                Objects.equals(weakness, hero.weakness);
+    }
