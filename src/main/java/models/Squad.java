795616c6dbe400a1824c6b1cb274d8d2bb3643e1
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-06-11 00:14:36 +0300

    Update Squad.java

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,14 @@
 package models;
-
+import java.util.ArrayList;
+import java.util.Objects;
 public class Squad {
-}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Squad squad = (Squad) o;
+        return maxNum == squad.maxNum &&
+                id == squad.id &&
+                Objects.equals(squadName, squad.squadName) &&
+                Objects.equals(cause, squad.cause);
+    }
